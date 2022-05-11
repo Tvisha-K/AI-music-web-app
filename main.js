8f3ae452ebb03a155532e2c2edcf4b7074ab06ae
Tvisha-K <79145925+Tvisha-K@users.noreply.github.com>
2022-05-11 14:05:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-music="";
+HP="";
+
+peter_pan = "";
+
+function setup(){
+
+    canvas = createCanvas(600,500);
+
+    canvas.center();
+
+    video = createCapture(VIDEO);
+
+    video.size(600,500);
+
+    video.hide();
+
+    pose_model =  ml5.poseNet(video,modelLoaded);
+
+}
+
+function modelLoaded(){
+
+console.log("Model has loaded successfully")
+
+
+}
+
+function draw(){
+
+    image(video,0,0,600,500);
+
+}
+
