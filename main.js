9087d93a8d5f22b0f8cf1d3b1e4f97decaa2bbc0
Tvisha-K <79145925+Tvisha-K@users.noreply.github.com>
2022-05-12 16:22:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
-HP="";
+harry="";
 
 peter_pan = "";
+
+leftX = "";
+
+leftY ="";
+
+rightX = "";
+
+rightY = "";
+
+function preload(){
+
+    harry = loadSound("music.mp3");
+
+    peter_pan = loadSound("music2.mp3");
+
+}
 
 function setup(){
 
@@ -15,6 +31,8 @@ function setup(){
     video.hide();
 
     pose_model =  ml5.poseNet(video,modelLoaded);
+
+    pose_model.on("pose",gotResults);
 
 }
 
@@ -31,3 +49,29 @@ function draw(){
 
 }
 
+function play(){
+
+    music.play();
+
+    music.setVolume(1);
+
+    music.rate(1);
+
+}
+
+function gotResults(result){
+
+    if (result.length > 0) {
+
+         console.log(result);
+ 
+         leftX = result[0].pose.leftWrist.x;
+ 
+         leftY = result[0].pose.leftWrist.y;
+ 
+         rightX = result[0].pose.rightWrist.x;
+ 
+         rightY= result[0].pose.rightWrist.y;
+    }
+
+}
